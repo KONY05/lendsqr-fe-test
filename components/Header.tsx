c7f8b4a278d1f58ebac16c9765e14eab2712d2c9
Odesanya Konyinsola <konyinsolaodesanya@gmail.com>
2025-07-17 17:03:01 +0100

    implement mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import { Input } from "./ui/input";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { nav_businesses, nav_customers, nav_settings } from "@/lib/constants";
+import { ChevronDown } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname.startsWith(`/${path}`);
   return (
-    <header className="flex w-full items-center justify-between gap-2 py-3 pr-4 pl-3.5 shadow-lg md:pr-9 md:pl-[30px] lg:py-[20px]">
+    <header className="flex w-full items-center justify-between gap-2 py-3 pr-4 pl-3.5 shadow-lg max-[321px]:pr-2.5 md:pr-9 md:pl-[30px] lg:py-[20px]">
       <div>
         <Image
           src="/logo.png"
@@ -13,7 +28,6 @@ function Header() {
           height={50}
           className=""
         />
-        {/* <Image src="/favicon.svg" alt="Lendsqr Logo" width={20} height={10} className="md:hidden" /> */}
       </div>
       <div>
         <div className="cursor-pointer rounded-full bg-[#39CDCC] p-2 md:hidden">
@@ -40,7 +54,7 @@ function Header() {
         </div>
       </div>
       <div className="flex items-center">
-        <p className="mr-[24px] cursor-pointer text-sm text-[#213F7D] underline md:mr-[47px] md:text-[16px]">
+        <p className="mr-[24px] cursor-pointer text-sm text-[#213F7D] underline max-[321px]:mr-[10px] md:mr-[47px] md:text-[16px]">
           Docs
         </p>
         <Image
@@ -58,7 +72,9 @@ function Header() {
             height={30}
             className="rounded-full"
           />
-          <p className="ml-2.5 text-sm font-medium md:text-[16px]">Adedeji</p>
+          <p className="ml-2.5 text-sm font-medium max-[321px]:text-[10px] md:text-[16px]">
+            Adedeji
+          </p>
           <Image
             src="/icons/down.svg"
             alt="dropdown icon"
@@ -68,7 +84,89 @@ function Header() {
         </div>
       </div>
       {/* TODO: implement mobile nav */}
-      <Menu className="lg:hidden" />
+
+      <Sheet>
+        <SheetTrigger>
+          <Menu className="cursor-pointer lg:hidden" />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="mb-0">
+            <SheetTitle>Navigation</SheetTitle>
+          </SheetHeader>
+          <section className="border-r bg-white pt-[20px] lg:hidden">
+            <p className="flex items-center gap-2.5 px-[30px] text-[#213F7D]">
+              <Image
+                src="/navIcons/briefcase.svg"
+                alt="nav icon"
+                width={15}
+                height={10}
+              />{" "}
+              Switch organization
+              <ChevronDown className="size-4" />
+            </p>
+            <p className="mt-[32px] flex items-center gap-2.5 px-[30px] text-[#213F7D] opacity-55">
+              <Image
+                src="/navIcons/briefcase.svg"
+                alt="nav icon"
+                width={15}
+                height={10}
+              />{" "}
+              Dashboard
+            </p>
+
+            <div className="mt-[41px] h-[65vh] space-y-[41px] overflow-y-auto">
+              <div>
+                <h2 className="px-[30px] text-xs font-medium text-[#213F7D] uppercase">
+                  Customers
+                </h2>
+                <div className="mt-2.5">
+                  {nav_customers.map(({ name, icon }) => (
+                    <p
+                      key={name}
+                      className={`flex cursor-pointer items-center gap-2.5 px-[30px] py-2.5 text-[#213F7D] ${isActive(name.toLowerCase()) ? "border-l-3 border-[#39CDCC] bg-[#39CDCC0F]" : "opacity-55"}`}
+                    >
+                      <Image src={icon} alt="nav icon" width={15} height={10} />{" "}
+                      {name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <h2 className="px-[30px] text-xs font-medium text-[#213F7D] uppercase">
+                  Businesses
+                </h2>
+                <div className="mt-2.5">
+                  {nav_businesses.map(({ name, icon }) => (
+                    <p
+                      key={name}
+                      className="flex cursor-pointer items-center gap-2.5 px-[30px] py-2.5 text-[#213F7D] opacity-55"
+                    >
+                      <Image src={icon} alt="nav icon" width={15} height={10} />{" "}
+                      {name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <h2 className="px-[30px] text-xs font-medium text-[#213F7D] uppercase">
+                  Settings
+                </h2>
+                <div className="mt-2.5">
+                  {nav_settings.map(({ name, icon }) => (
+                    <p
+                      key={name}
+                      className="flex cursor-pointer items-center gap-2.5 px-[30px] py-2.5 text-[#213F7D] opacity-55"
+                    >
+                      <Image src={icon} alt="nav icon" width={15} height={10} />{" "}
+                      {name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 }
